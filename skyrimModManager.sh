07356267c348183f7819aab0a1f9b18a6ac336bb
Lukas H <lukas@ideallygrey.uk>
2023-08-27 12:59:23 -0400

    Include gum Rather than downloading gum afterwards, it'll simply come included

--- skyrimModManager.sh
+++ skyrimModManager.sh
@@ -10,12 +10,13 @@ export GUM_CHOOSE_CURSOR_FOREGROUND="4"
 export GUM_CONFIRM_SELECTED_BACKGROUND="4"
 export GUM_CONFIRM_SELECTED_BOLD="true"
 
+
 func_launch_game () {
-	./gum confirm "Launch Game?" && ./gum spin steam steam://rungameid/$skyrimSteamAppID --spinner="line" --title="Loading..." && exit || func_main_menu
+	./gum/gum confirm "Launch Game?" && ./gum/gum spin steam steam://rungameid/$skyrimSteamAppID --spinner="line" --title="Loading..." && exit || func_main_menu
 }
 
 func_modlist_options_menu () {
-	case $(./gum choose "Apply new modlist(s)" "Remove current modlist" "Back to main menu" --header="Modlists" --cursor=" >> ") in 
+	case $(./gum/gum choose "Apply new modlist(s)" "Remove current modlist" "Back to main menu" --header="Modlists" --cursor=" >> ") in 
 		"Apply new modlist")
 			printf "Applying new modlist"
 			;;
@@ -29,7 +30,7 @@ func_modlist_options_menu () {
 }
 
 func_main_menu () {
-	case $(./gum choose "Launch Skyrim!" "Change applied modlist" "Exit" --header="Skyrim Mod Manager" --header.border="rounded" --cursor=" >> ") in
+	case $(./gum/gum choose "Launch Skyrim!" "Change applied modlist" "Exit" --header="Skyrim Mod Manager" --header.border="rounded" --cursor=" >> ") in
 		"Launch Skyrim!")
 			func_launch_game
 			;;
@@ -51,13 +52,6 @@ if [[ $(ls | grep -w -o skyrimModManager.sh) == "" ]]
 	exit
 fi
 
-# Checks if gum is installed or if binary was previously downloaded
-if [[ $(whereis gum) == "gum:" ]] && [[ $(ls | grep -w -o gum) == "" ]]
-	then
-	printf "Fetching dependency..."
-	curl -OLs https://github.com/charmbracelet/gum/releases/download/v0.11.0/gum_0.11.0_Linux_x86_64.tar.gz
-	tar -xzf gum_0.11.0_Linux_x86_64.tar.gz gum
-	printf " Done!\n"
-fi
+
 
 func_main_menu
